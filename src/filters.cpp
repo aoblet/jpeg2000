9ba59bbe97df872d6d5d0cdb5de392d1d7d6f2c4
Alexis Oblet <alexis.oblet@gmail.com>
2015-10-27 23:28:14 +0100

    fix synthesisHaar and update signal api
    - fix convol: inclusive max
    - fix synthesis: interpolate before convol
    - use assert for odd/even signals

--- src/filters.cpp
+++ src/filters.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <assert.h>
 #include "filters.hpp"
 
 namespace jpeg2000{ namespace filters{
@@ -16,16 +17,19 @@ namespace jpeg2000{ namespace filters{
         int halfSize = signal.size() / 2;
 
         for (int i = 0; i < halfSize; ++i)
-            decimated.push_back(signal[i << 1]); // <<1 == *2
+            decimated.push_back(signal[i*2]);
         return decimated;
     }
 
     Signal1D convol(const Signal1D &signal, const Signal1D &filter) {
+        // first check the filter size is odd
+        assert(filter.size() % 2);
+
         int sizeSignal = signal.size(), sizeFilter = filter.size(), halfSizeFilter = sizeFilter/2;
         Signal1D filtered(sizeSignal);
 
         for (int n = 0; n < sizeSignal; ++n) {
-            for (int k = -halfSizeFilter; k < halfSizeFilter; ++k) {
+            for (int k = -halfSizeFilter; k <= halfSizeFilter; ++k) {
                 // handle mirror symmetry
                 filtered[n] += filter[k + (halfSizeFilter)] * mirrorSymmetry(signal, n - k);
             }
@@ -59,7 +63,8 @@ namespace jpeg2000{ namespace filters{
 
     Signal1D analyseHaar97(const Signal1D &signal) {
         Signal1D _h0,_h1;
-        // Filtre biorthogonal 9/7 _h0 (longueur 9)
+
+        // biorthogonal filter 9/7 _h0 (length 9)
         _h0.push_back(0.037828455507  );
         _h0.push_back(-0.023849465019 );
         _h0.push_back(-0.110624404418 );
@@ -70,7 +75,7 @@ namespace jpeg2000{ namespace filters{
         _h0.push_back(-0.023849465019 );
         _h0.push_back(0.037828455507  );
 
-        // Filtre biorthogonal 9/7 _h1 (longueur 9)
+        // biorthogonal filter 9/7 _h1 (length 9)
         _h1.push_back(0.064538882629 );
         _h1.push_back(-0.040689417610);
         _h1.push_back(-0.418092273222);
@@ -87,7 +92,7 @@ namespace jpeg2000{ namespace filters{
     Signal1D synthesisHaar97(const Signal1D &haarSignal) {
         Signal1D _g0,_g1;
 
-        // Filtre biorthogonal 9/7 _g0 (longueur 7)
+        // biorthogonal filter 9/7 _g0 (length 7)
         _g0.push_back(-0.064538882629);
         _g0.push_back(-0.040689417610);
         _g0.push_back(0.418092273222 );
@@ -96,7 +101,7 @@ namespace jpeg2000{ namespace filters{
         _g0.push_back(-0.040689417610);
         _g0.push_back(-0.064538882629);
 
-        // Filtre biorthogonal 9/7 _g1 (longueur 11)
+        // biorthogonal filter 9/7 _g1 (length 11)
         _g1.push_back(0.000000000000 );
         _g1.push_back(-0.000000000000);
         _g1.push_back(0.037828455507 );
@@ -112,6 +117,9 @@ namespace jpeg2000{ namespace filters{
     }
 
     Signal1D _analyseHaar(const Signal1D &inSignal, const Signal1D &_h0, const Signal1D &_h1) {
+        // first check that the signal is even
+        assert(!(inSignal.size()%2));
+
         // sld == signalLowPassDecimated
         // shd == signalHighPassDecimated
         Signal1D sld(decimation2(convol(inSignal, _h0))),
@@ -120,13 +128,17 @@ namespace jpeg2000{ namespace filters{
     }
 
     Signal1D _synthesisHaar(const Signal1D &analysedSignal, const Signal1D &_g0, const Signal1D &_g1) {
+        // first check that the signal is even
+        assert(!(analysedSignal.size()%2));
+
         Signal1D approximateCoeffs(analysedSignal.extract(0, analysedSignal.size()/2 - 1));
         Signal1D detailsCoeffs(analysedSignal.extract(analysedSignal.size()/2));
 
         // sli == Signal LowPass  Interpolated
         // shi == Signal HighPass Interpolated
-        Signal1D sli(interpole2(convol(approximateCoeffs, _g0))),
-                 shi(interpole2(convol(detailsCoeffs, _g1)));
+        Signal1D sli(convol(interpole2(approximateCoeffs), _g0)),
+                 shi(convol(interpole2(detailsCoeffs), _g1));
+
 
         return sli += shi ;  // reconstructed signal
     }
